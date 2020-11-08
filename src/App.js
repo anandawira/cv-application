1771c17f92da2d3ec0c4ed5868e4dcd4242c0d7b
Ananda Wiradharma <anandawira754@gmail.com>
2020-11-08 19:07:35 +0700

    cv

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import General from './components/General';
+import Education from './components/Education';
+import Experience from './components/Experience';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="">
+      <div id="cv" className="col-12 col-sm-8 col-md-6 col-lg-5 mx-auto border">
+        <General />
+        <Education />
+        <Experience />
+      </div>
+    </div>
+  );
 }
 
 export default App;
